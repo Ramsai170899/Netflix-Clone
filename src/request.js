d58bbbb55c47da04f185ddd9f1a101552b08cf8d
Ramsai Jagadish <81312213+Ramsai170899@users.noreply.github.com>
2023-10-13 13:02:17 +0530

    Add files via upload

---
+++ src/request.js
@@ -0,0 +1,17 @@
+const API_KEY = "c076b2e15f4d6b3dd4e7a3c3e33c123d";
+
+
+
+const requests = {
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchDocumantaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+}
+
+
+export default requests;
